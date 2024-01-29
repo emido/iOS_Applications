4f06e5a0121a4361f22b43858ad475a0c2767595
Maryam.Emami <m.emami@github.com>
2024-01-29 11:44:13 -0500

    test app commit

---
+++ testapp/testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Maryam on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
